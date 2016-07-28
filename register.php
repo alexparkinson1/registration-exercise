59efae1948d7374293605f21711beb5d2afa3a8d
Alex <alexeparkinson@gmail.com>
2016-07-28 21:21:06 +0100

    added images and styling for desktop on homepage and welcome page

--- register.php
+++ register.php
@@ -1,11 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta charset="utf-8">
+    <title>Thank you</title>
+  </head>
+  <body>
+    <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
 
-      echo "Thank you for registering, $email";
+      echo "<p class='response'>Thank you for registering, $email<p>";
 
     } else {
       http_response_code(403);
-      echo 'Whoops, something went wrong, please try again';
+      echo "<p class='response'>Whoops, something went wrong! Please try again</p>";
     }
-?>
+    ?>
+  </body>
+</html>
