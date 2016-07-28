05fbd99fa5a07237dce0e00408325a90bc92eed9
Alex <alexeparkinson@gmail.com>
2016-07-28 20:58:29 +0100

    added the jquery date picker for the birthday field

--- register.php
+++ register.php
@@ -3,5 +3,9 @@
       $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
 
       echo "Thank you for registering, $email";
+
+    } else {
+      http_response_code(403);
+      echo 'Whoops, something went wrong, please try again';
     }
 ?>
--- script.js
+++ script.js
@@ -2,8 +2,18 @@ $(document).ready(function () {
   $(function() {
     var form = $('#register');
 
+    $('#birthday').datepicker({
+      maxDate: new Date,
+      changeMonth: true,
+      changeYear: true,
+      yearRange: "-100Y:-0Y"
+    });
+
     $(form).submit(function(event) {
       var formData = $(form).serialize();
+
+      $('#register-button').prop('disabled', true);
+      $('#register-button').addClass('disabled');
 
       $.ajax({
         type: 'POST',
